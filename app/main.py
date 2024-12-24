0f527a717eb677bedb34a7ead7f60c304e90afcb
Mojeeb2002 <mojeebdaffallah38@gmail.com>
2024-12-25 00:16:55 +0300

    vercel.json added

--- app/main.py
+++ app/main.py
@@ -31,3 +31,7 @@ app.include_router(jobsRoutes.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
